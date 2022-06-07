a470723605fa7a9eb5aa64e47f79a9c29c184f82
hemanth <hemanthkumar3461@gmail.com>
2022-06-07 23:03:25 +0530

    Sample code

--- src/Basics/HelloWorld.java
+++ src/Basics/HelloWorld.java
@@ -3,5 +3,6 @@ package Basics;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world buddy");
+        System.out.println("Hello world buddy");
     }
 }
